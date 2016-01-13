079f86d18e79b4ddc2341aa6336278221b68a7be
lyjw <jaynellew@gmail.com>
2016-01-13 14:36:38 +0000

    Completed OOP:Rock,Paper,Scissors

---
+++ oop_rps.rb
@@ -0,0 +1,141 @@
+module Formattable
+  def format(line)
+    puts "=> #{line}"
+  end
+end
+
+class Hand
+  include Comparable
+
+  attr_reader :choice
+
+  CHOICES = {"r" => "Rock", "p" => "Paper", "s" => "Scissors"}
+
+  def initialize(choice)
+    @choice = choice
+  end
+
+  def <=>(other_hand)
+    if @choice == other_hand.choice
+      0
+    elsif (@choice == 'r' && other_hand.choice == 's') || (@choice == 'p' && other_hand.choice == 'r') || (@choice == 's' && other_hand.choice == 'p')
+      1
+    else
+      -1
+    end
+  end
+
+  def display_winning_result
+    case @choice
+    when 'r'
+      puts "Rock smashes Scissors!"
+    when 'p'
+      puts "Paper smothers Rock!"
+    when 's'
+      puts "Scissors shreds Paper!"
+    end
+  end
+end
+
+class Player
+  attr_accessor :name, :hand, :wins
+
+  def initialize(name = "Computer")
+    @name = name
+    @wins = 0
+  end
+
+  def to_s
+    "#{name} chose #{Hand::CHOICES[self.hand.choice]}"
+  end
+
+  def pick_hand
+    self.hand = Hand.new(Hand::CHOICES.keys.sample)
+  end
+end
+
+class Human < Player
+  include Formattable
+
+  def pick_hand
+    begin
+      format "Pick one: (R / P / S)"
+      choice = gets.chomp.downcase
+    end until Hand::CHOICES.keys.include?(choice)
+
+    self.hand = Hand.new(choice)
+  end
+end
+
+class Game
+  include Formattable
+
+  attr_accessor :player, :computer, :tie
+
+  def initialize
+    @player = Human.new("Player")
+    @computer = Player.new
+    @rounds = 0
+    @tie = 0
+  end
+
+  def welcome_message
+    system 'clear'
+
+    format "Welcome to Rock, Paper, Scissors!\n\n"
+    puts 'What is your name?'
+    player.name = gets.chomp
+  end
+
+  def compare_hands
+    if player.hand == computer.hand
+      @tie += 1
+      format "It's a tie!"
+    elsif player.hand > computer.hand
+      player.wins += 1
+      player.hand.display_winning_result
+      format "#{player.name} won!"
+    else
+      computer.hand.display_winning_result
+      computer.wins += 1
+      format "#{computer.name} won!"
+    end
+  end
+
+  def replay?
+    puts"\nPlay again? (Enter 'Y' for another round or any key to exit)"
+    gets.chomp.downcase
+  end
+
+  def display_score
+    scoreboard = <<-EOL
+
+    Score (out of #{@rounds} rounds) -
+    #{player.name}: #{player.wins} wins
+    Computer: #{computer.wins} wins
+    Tie: #{@tie}
+
+    EOL
+
+    puts scoreboard
+  end
+
+  def run
+    welcome_message
+
+    begin 
+      system 'clear'
+
+      @rounds += 1
+      player.pick_hand
+      puts player
+      computer.pick_hand
+      puts computer
+      compare_hands
+    end until replay? != 'y'
+
+    display_score
+  end
+end 
+
+Game.new.run
